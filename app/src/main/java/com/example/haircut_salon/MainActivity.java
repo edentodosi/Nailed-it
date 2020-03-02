44de7e18070c8f70692e98a667e43d95d31d92cb
edentodosi <edentodosi996@gmail.com>
2020-03-03 00:21:56 +0200

    try of passing text between activities not complete spinner methods

--- app/src/main/java/com/example/haircut_salon/MainActivity.java
+++ app/src/main/java/com/example/haircut_salon/MainActivity.java
@@ -7,13 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     private Button button;
-
+    EditText et;
+    String st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)   {
@@ -22,22 +24,34 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Spinner spinner_nail=findViewById(R.id.spinner);
         spinner_nail.setOnItemSelectedListener(this);
         button= (Button) findViewById(R.id.button);
+        et= findViewById(R.id.putnamehere);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
+                Intent i=new Intent(MainActivity.this,Schedule_time.class);
+                //openActivity2();
+                st=et.getText().toString();
+                i.putExtra("value",st);
+                startActivity(i);
+                finish();
+
+
             }
         });
     }
 
-    public void openActivity2(){
-        Intent intent= new Intent(this, Schedule_time.class);
-        startActivity(intent);
-    }
+    //public void openActivity2(){
+    //Intent intent= new Intent(this, Schedule_time.class);
+     //   startActivity(intent);}
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(this,parent.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
--- app/src/main/java/com/example/haircut_salon/Schedule_time.java
+++ app/src/main/java/com/example/haircut_salon/Schedule_time.java
@@ -6,15 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Schedule_time extends AppCompatActivity {
     private Button button;
+    TextView tv;
+    String st;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_time);
         button= (Button) findViewById(R.id.button);
+        tv=findViewById(R.id.getname);
+        st=getIntent().getExtras().getString("Value");
+        tv.setText(st);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
