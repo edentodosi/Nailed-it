b8d8966516e8ae26effab5312e260420ec58f475
edentodosi <edentodosi996@gmail.com>
2020-03-07 19:32:57 +0200

    localization to Hebrew implantation all buttons

--- app/src/main/java/com/example/nailit/Schedule_time.java
+++ app/src/main/java/com/example/nailit/Schedule_time.java
@@ -2,13 +2,18 @@ package com.example.nailit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.nailit.NailTypes.AcrylicPolish;
@@ -17,10 +22,15 @@ import com.example.nailit.NailTypes.INailOptions;
 import com.example.nailit.NailTypes.Polish;
 import com.example.nailit.NailTypes.PolishRemover;
 
+import java.util.Calendar;
+import java.util.Timer;
+
 public class Schedule_time extends AppCompatActivity {
     private Button button;
     TextView tv;
     String st;
+    TextView mtv;
+    TextView htv;
 
     INailOptions[] nailOptions = { new Polish(), new Gel(), new AcrylicPolish(), new PolishRemover()};
 
@@ -44,6 +54,40 @@ public class Schedule_time extends AppCompatActivity {
                 openActivity3();
             }
         });
+
+        mtv=(TextView)findViewById(R.id.date_picker_title);
+        ImageButton bt=(ImageButton)findViewById(R.id.imageButton2);
+        htv=(TextView)findViewById(R.id.houer_picker_title);
+        ImageButton hbt=(ImageButton)findViewById(R.id.clock2);
+
+        hbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c=Calendar.getInstance();
+                int day=c.get(Calendar.DAY_OF_MONTH);
+                int month=c.get(Calendar.MONTH);
+                int year=c.get(Calendar.YEAR);
+                DatePickerDialog dpd=new DatePickerDialog(Schedule_time.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mtv.setText(dayOfMonth+"/"+ (month+1) + "/"+ year);
+                    }
+                },day,month,year);
+                dpd.show();
+
+                int hour=c.get(Calendar.HOUR_OF_DAY);
+                int minutes=c.get(Calendar.MINUTE);
+                TimePickerDialog tpd=new TimePickerDialog(Schedule_time.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        htv.setText(hourOfDay+":"+ minute);
+                    }
+                },hour,minutes,true);
+                tpd.show();
+            }
+
+        });
+
     }
         public void openActivity3(){
             CheckBox agree = findViewById(R.id.agree);
--- app/src/main/java/com/example/nailit/contact_option.java
+++ app/src/main/java/com/example/nailit/contact_option.java
@@ -1,8 +1,16 @@
 package com.example.nailit;
-
+//taken from https://developer.android.com/training/basics/intents/sending
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.net.URI;
+
+import static java.net.Proxy.Type.HTTP;
 
 public class contact_option extends AppCompatActivity {
 
@@ -10,5 +18,41 @@ public class contact_option extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_option);
+        ImageButton bt=(ImageButton)findViewById(R.id.imageButton2_contact);
+        ImageButton wbt=(ImageButton)findViewById(R.id.clock2_contact_rec);
+        ImageButton ebt=(ImageButton)findViewById(R.id.clock2_contact);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri number = Uri.parse("tel:053-1234567");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                startActivity(callIntent);
+            }
+        });
+
+        wbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://www.google.com/search?q=nailed+it&rlz=1C1SQJL_enIL772IL772&oq=nai" +
+                        "led+&aqs=chrome.1.69i57j0l7.5260j0j8&sourceid=chrome&ie=UTF-8");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+        ebt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"Nailedit@gmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Special request");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "My special request is- ");
+                startActivity(emailIntent);
+
+            }
+        });
+
+
     }
 }
