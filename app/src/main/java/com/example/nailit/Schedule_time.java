1a605551a36bc0049ec368190a725083aa680880
idanperez <ipdan96@gmail.com>
2020-03-22 20:13:06 +0200

    added asyncTask get weather

--- app/src/main/java/com/example/nailit/Schedule_time.java
+++ app/src/main/java/com/example/nailit/Schedule_time.java
@@ -6,7 +6,9 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -22,27 +24,41 @@ import com.example.nailit.NailTypes.INailOptions;
 import com.example.nailit.NailTypes.Polish;
 import com.example.nailit.NailTypes.PolishRemover;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Calendar;
-import java.util.Timer;
 
 public class Schedule_time extends AppCompatActivity {
     private Button button;
     TextView tv;
+    TextView weatherTv;
     String st;
     TextView mtv;
     TextView htv;
 
-    INailOptions[] nailOptions = { new Polish(), new Gel(), new AcrylicPolish(), new PolishRemover()};
-
+    INailOptions[] nailOptions = { new Polish(), new Gel(), new AcrylicPolish()};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        EdensAsyncTask getWeather = new EdensAsyncTask();
+        getWeather.execute("https://api.darksky.net/forecast/a9f37e081e9648fac0fbafde5cb379f0/32.016110,34.773010?units=auto");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_time);
         button= (Button) findViewById(R.id.button);
         tv=findViewById(R.id.client_title_name);
+        weatherTv = findViewById(R.id.weahter_title);
         st=getIntent().getStringExtra("value");
-        tv.setText("Hey " +st +", pick your wanted time");
+        //getResources().getString(R.string.);
+        tv.setText("Hey " + st +", pick your wanted time");
 
         TextView priceTv = findViewById(R.id.price_summary);
         int nailType=getIntent().getIntExtra("nailPolish", 0);
@@ -57,7 +73,7 @@ public class Schedule_time extends AppCompatActivity {
 
         mtv=(TextView)findViewById(R.id.date_picker_title);
         ImageButton bt=(ImageButton)findViewById(R.id.imageButton2);
-        htv=(TextView)findViewById(R.id.houer_picker_title);
+        htv=(TextView)findViewById(R.id.hour_picker_title);
         ImageButton hbt=(ImageButton)findViewById(R.id.clock2);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,33 +93,85 @@ public class Schedule_time extends AppCompatActivity {
         });
 
         hbt.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Calendar c = Calendar.getInstance();
-                    int hour=c.get(Calendar.HOUR_OF_DAY);
-                    int minutes=c.get(Calendar.MINUTE);
-                    TimePickerDialog tpd=new TimePickerDialog(Schedule_time.this, new TimePickerDialog.OnTimeSetListener() {
-                        @Override
-                        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                            htv.setText(hourOfDay+":"+ minute);
-                        }
-                    },hour,minutes,true);
-                    tpd.show();
-                }
-            });
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                int hour=c.get(Calendar.HOUR_OF_DAY);
+                int minutes=c.get(Calendar.MINUTE);
+                TimePickerDialog tpd=new TimePickerDialog(Schedule_time.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        htv.setText(hourOfDay+":"+ minute);
+                    }
+                },hour,minutes,true);
+                tpd.show();
+            }
+        });
+    }
 
+    public void openActivity3(){
+        CheckBox agree = findViewById(R.id.agree);
+        if(!agree.isChecked()){
+            Toast toast = Toast.makeText(this, "You must agree the terms first", Toast.LENGTH_LONG);
+            TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+            v.setTextColor(Color.RED);
+            toast.show();
+            return;
         }
-        public void openActivity3(){
-            CheckBox agree = findViewById(R.id.agree);
-            if(!agree.isChecked()){
-                Toast toast = Toast.makeText(this, "You must agree the terms first", Toast.LENGTH_LONG);
-                TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                v.setTextColor(Color.RED);
-                toast.show();
+        Intent intent= new Intent(this, contact_option.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private class EdensAsyncTask extends AsyncTask<String, Integer, Double> {
+        @Override
+        protected Double doInBackground(String... params) {
+            // call the server and return response
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                }
+
+                String result = buffer.toString();
+                JSONObject obj = new JSONObject(result);
+                JSONObject currentWeather = obj.getJSONObject("currently");
+                Double temp = currentWeather.getDouble("temperature");
+                return temp;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return -999.0;
+        }
+
+        @Override
+        protected void onPostExecute(Double weather){
+            if(weather == -999.0){
                 return;
             }
-            Intent intent= new Intent(this, contact_option.class);
-            startActivity(intent);
-            finish();
+            weatherTv.setText(getResources().getString(R.string.current_weather) + weather.toString());
         }
     }
+}
+
